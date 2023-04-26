d311b47b5e7a0bdecaf91b942c90c4d292173ff0
Luai Okasha <luai.okasha@infineon.com>
2023-04-26 16:14:33 +0200

    some methods implementation!

--- Sync_Pool.py
+++ Sync_Pool.py
@@ -1,13 +1,18 @@
+from Sync_Filter import Sync_Filter
+
 class Sync_Pool:
+
     def __init__(self, path):
-        _pool_path = path
+        self._pool_path = path
         pass
     
+    def filter_pool(self, filter : Sync_Filter):
+        filter.apply_filter() # todo
+        pass
     def init_pool(self):
         pass
-    def request_merge(self, merge_to_url):
+    def clone_from_source(self, source_repo_url):
         pass
-
-    def locate_repo(self,):
+    def merge_to_target(self, target_repo_url):
         pass
     pass
