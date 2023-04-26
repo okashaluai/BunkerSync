dd495b7b51bb2c8ae344a4b49bce3d783eaecbc4
Luai Okasha <luai.okasha@infineon.com>
2023-04-26 16:13:13 +0200

    synchronizer was implemented!

--- Synchronizer.py
+++ Synchronizer.py
@@ -1,10 +1,26 @@
-import Sync_Pool
-import Sync_Filter
-
+from Sync_Pool import Sync_Pool
+from Deletion_Filter import deletion_filter
+import os 
+from pathlib import Path
 class Synchronizer:
 
+
     def __init__(self):
-        _sync_pool = Sync_Pool()
-        _sync_filter = Sync_Filter()
+        pool_path = Path('./pool')
+        config_path = Path('./config')
+        if not os.path.exists(pool_path):
+            os.mkdir(pool_path)
+        if not os.path.exists(config_path):
+            os.mkdir(config_path)
+        if not os.path.exists(config_path / 'roadside.txt'):
+            open(config_path / 'roadside.txt', "x")
+        self._sync_pool = Sync_Pool(pool_path)
         pass
-    pass
+
+    def run(self, source_repo_url, target_repo_url):
+        self._sync_pool.clone_from_source(source_repo_url)
+        filter = deletion_filter()
+        self._sync_pool.filter_pool(filter)
+        self._sync_pool.merge_to_target(target_repo_url)
+        pass
+
