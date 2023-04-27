787d43b4abecf553b5441c83897654e7c908a8ed
Luai Okasha <luai.okasha@infineon.com>
2023-04-27 11:46:05 +0200

    clone source and filter pool was done!

--- Sync_Pool.py
+++ Sync_Pool.py
@@ -1,6 +1,7 @@
 from Sync_Filter import Sync_Filter
 import os
 import subprocess
+import pathlib
 
 class Sync_Pool:
 
@@ -10,16 +11,21 @@ class Sync_Pool:
     
     def filter_pool(self, branch, filter : Sync_Filter):
         #switch to branch
+        # curr_dir = pathlib.Path(os.getcwd())
+
+        # repo_path = list(curr_dir.iterdir())[0] # I know that a repo is clone into one dir.
+        # #os.chdir(repo_path)
+        # print(repo_path)
+        # subprocess.run(['git', '-C', repo_path,  'checkout', branch], shell=True)
+
         filter.apply_filter(self._pool_path) # todo
         pass
     def init_pool(self):
         pass
     def clone_from_source(self, source_repo_url):
-        os.chdir(self._pool_path)
-        subprocess.run(['git', 'clone', source_repo_url], shell=True)
-        output = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'])
-        print(output)
-
+        # os.chdir(self._pool_path)
+        subprocess.run(['git', 'clone', source_repo_url, self._pool_path], shell=True)
+        
     def merge_to_target(self, target_repo_url):
         pass
     pass
