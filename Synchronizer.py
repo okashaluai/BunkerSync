9c68cb649a9a142fc461791621e261da56fc6385
Luai Okasha <luai.okasha@infineon.com>
2023-04-28 18:27:19 +0200

    minor changes

--- Synchronizer.py
+++ Synchronizer.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class Synchronizer:
     def __init__(self):
         pool_path = Path('./pool')
-        config_path = Path('./config')
+        config_path = Path('./sync_config')
         if not os.path.exists(pool_path):
             os.makedirs(pool_path)
         if not os.path.exists(config_path):
@@ -15,11 +15,11 @@ class Synchronizer:
         self._sync_pool = Sync_Pool(pool_path)
         pass
 
-    def run(self, source_repo_url, target_repo_url, branch):
-        self._sync_pool.clone_from_source(branch, source_repo_url)
+    def run(self, src_repo_url, dst_repo_url, branch_name):
+        self._sync_pool.clone_src_dst(src_repo_url, dst_repo_url, branch_name)
         filter = deletion_filter()
-        self._sync_pool.filter_pool(filter)
-        self._sync_pool.merge_to_target(target_repo_url, branch)
+        self._sync_pool.filter_dst_pool(filter)
+        self._sync_pool.merge_to_dst(src_repo_url)
         self._sync_pool.clean_pool()
         pass
 
