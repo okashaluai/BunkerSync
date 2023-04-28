aee757dd4f724b6df055ebb529a22e6359034fde
Luai Okasha <luai.okasha@infineon.com>
2023-04-28 14:55:58 +0200

    command line args have been added!

--- reposync.py
+++ reposync.py
@@ -1,15 +1,21 @@
 from Synchronizer import Synchronizer
 import os
-def main():
-    source_repo_url = 'https://github.com/okashaluai/source.git'
-    target_repo_url = 'https://github.com/okashaluai/target-repo.git'
-    branch = 'b1'
+import sys
+def main(args):
+    if (len(args)) is 3:
+        source_repo_url = args[0]
+        target_repo_url = args[1] 
+        branch = args[2]
+    else:
+        source_repo_url = 'https://github.com/okashaluai/source.git'
+        target_repo_url = 'https://github.com/okashaluai/target-repo.git'
+        branch = 'b1'
 
     Synchronizer().run(source_repo_url, target_repo_url, branch)
 
     input('Press any key to exit.')
     
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
 
  
