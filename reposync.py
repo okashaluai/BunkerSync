55980fde2d28c39e543a2921b06fd004504b6dd5
Luai Okasha <luai.okasha@infineon.com>
2023-04-28 18:26:34 +0200

    added args

--- reposync.py
+++ reposync.py
@@ -2,16 +2,16 @@ from Synchronizer import Synchronizer
 import os
 import sys
 def main(args):
-    if (len(args)) is 3:
-        source_repo_url = args[0]
-        target_repo_url = args[1] 
+    if (len(args)) == 3:
+        src_repo_url = args[0]
+        dst_repo_url = args[1] 
         branch = args[2]
     else:
-        source_repo_url = 'https://github.com/okashaluai/source.git'
-        target_repo_url = 'https://github.com/okashaluai/target-repo.git'
+        src_repo_url = 'https://github.com/okashaluai/source.git'
+        dst_repo_url = 'https://github.com/okashaluai/target-repo.git'
         branch = 'b1'
-
-    Synchronizer().run(source_repo_url, target_repo_url, branch)
+    
+    Synchronizer().run(src_repo_url, dst_repo_url, branch)
 
     input('Press any key to exit.')
     
