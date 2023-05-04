b22c81b3327ac855bcbe5b91f5904a11542a09e9
Luai Okasha <luai.okasha@infineon.com>
2023-05-04 14:45:06 +0200

    Documented!

--- Sync_Filter.py
+++ Sync_Filter.py
@@ -7,5 +7,11 @@ class Sync_Filter(ABC):
 
     @abstractmethod
     def apply_filter(self, path_to_filter):
+        """
+        Abstract method that enables implementing it as a filter application given a directory to filter.
+
+        Args:
+            path_to_filter (Path): path of directory to filter its content.
+        """
         pass
     pass
