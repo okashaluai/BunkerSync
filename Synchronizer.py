fb720a84ee07e40d0c4256c40b60d19f82e8adbb
Luai Okasha <luai.okasha@infineon.com>
2023-05-04 14:30:43 +0200

    Implementation was connected!

--- Synchronizer.py
+++ Synchronizer.py
@@ -14,13 +14,27 @@ class Synchronizer:
             open(config_path / 'roadside.txt', "x")
         self._sync_pool = Sync_Pool(pool_path)
         pass
-
-    def run(self, src_repo_url, dst_repo_url, branch_name):
+    
+    def sync_internal_external(self, src_repo_url, dst_repo_url, branch_name):
         self._sync_pool.clone_src_dst(src_repo_url, dst_repo_url, branch_name)
         self._sync_pool.local_merge()
         filter = deletion_filter()
+        self._sync_pool.filter_src_pool(filter)
         self._sync_pool.filter_dst_pool(filter)
         self._sync_pool.merge_to_dst(src_repo_url)
         self._sync_pool.clean_pool()
         pass
 
+    def sync_external_internal(self, internal_repo_url, external_repo_url):
+        self._sync_pool.clone_src(internal_repo_url)
+        self._sync_pool.clone_dst(external_repo_url)
+        self._sync_pool.pull_new_branches(internal_repo_url)
+        self._sync_pool.clean_pool()
+        pass
+
+
+    def run(self, src_repo_url, dst_repo_url, branch_name):
+        # self.sync_internal_external(src_repo_url, dst_repo_url, branch_name)
+        self.sync_external_internal(src_repo_url, dst_repo_url)
+    
+    
--- reposync.py
+++ reposync.py
@@ -1,21 +1,23 @@
 from Synchronizer import Synchronizer
-import os
 import sys
+import utils
+
 def main(args):
     if (len(args)) == 3:
         src_repo_url = args[0]
         dst_repo_url = args[1] 
         branch = args[2]
     else:
-        src_repo_url = 'https://github.com/okashaluai/source.git'
-        dst_repo_url = 'https://github.com/okashaluai/target-repo.git'
+        src_repo_url = 'https://github.com/okashaluai/internal.git'
+        dst_repo_url = 'https://github.com/okashaluai/external.git'
         branch = 'b1'
     
-    Synchronizer().run(src_repo_url, dst_repo_url, branch)
+    # Synchronizer().sync_internal_external(src_repo_url, dst_repo_url, branch)
+    Synchronizer().sync_external_internal(src_repo_url, dst_repo_url)
 
+    # utils.move_dir('./temp/.git', './dst')
     input('Press any key to exit.')
-    
+     
 if __name__ == "__main__":
     main(sys.argv)
 
- 
