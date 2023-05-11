d3f72adbdfa2a0b6a1276a4174b77486d90335c9
Luai Okasha <luai.okasha@infineon.com>
2023-05-11 13:44:02 +0200

    CLI options were fixed!

--- Dev/BunkerSync.py
+++ Dev/BunkerSync.py
@@ -9,13 +9,13 @@ def main():
         description =   'Welcome to BunkerSync tool!')
     
     
-    parser.add_argument('-p', '--publish', action='store_true')
-    parser.add_argument('-f', '--fetch', action='store_true')
+    parser.add_argument('-p', '--publish', action='store_true', help='Option to publish a given branch from internal repository to external repository.')
+    parser.add_argument('-f', '--fetch', action='store_true', help='Option to fetch all new branches with a given prefix from external repository to the internal repository.')
     parser.add_argument('-i', '--internal',action='store',default=None,  type = str, help = 'Internal repository url.')
     parser.add_argument('-e', '--external',action='store', default=None, type = str, help = 'External repository url.')
-    parser.add_argument('-b', '--branch', metavar='branch',default= None, action='store', type = str, help = '(Required when -p is on) Name of internal branch to be published to external repository.')
-    parser.add_argument('-@', '--prefix', metavar = 'prefix',default= None, action='store', type = str, help = '(Required when -f is on) Prefix of external branches to be pulled to internal repository.')
-    parser.add_argument('-fm', '--filter_map', metavar = 'filter_map_path',default= None, action='store', type=str, help = 'Path of the filtering map that includes files or folders names to be filtered.')
+    parser.add_argument('-b', '--branch', default= None, action='store', type = str, help = '(Required when -p is on) Name of internal branch to be published to external repository.')
+    parser.add_argument('-@', '--prefix', default= None, action='store', type = str, help = '(Required when -f is on) Prefix of external branches to be pulled to internal repository.')
+    parser.add_argument('-fm', '--filter_map', default= None, action='store', type=str, help = 'Path of the filtering map that includes files or folders names to be filtered.')
     args = parser.parse_args()
 
     def check_urls():
