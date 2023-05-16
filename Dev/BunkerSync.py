39d00bd7c002c1a6b99d3c46ae63403b08f54c79
Luai Okasha <130397131+okashaluai@users.noreply.github.com>
2023-05-16 10:06:33 +0000

    CLI options were added!

--- Dev/BunkerSync.py
+++ Dev/BunkerSync.py
@@ -16,9 +16,9 @@ def main():
     parser.add_argument('-b', '--branch', default= None, action='store', type = str, help = '(Required when -p is on) Name of internal branch to be published to external repository.')
     parser.add_argument('-@', '--prefix', default= None, action='store', type = str, help = '(Required when -f is on) Prefix of external branches to be pulled to internal repository.')
     parser.add_argument('-fm', '--filter_map', default= None, action='store', type=str, help = 'Path of the filtering map that includes files or folders names to be filtered.')
-    parser.add_argument('-sb', '--src_branch', default = None, type = str, help = '') #todo
-    parser.add_argument('-db', '--dst_branch', default = None, type = str, help = '')#todo
-    parser.add_argument('-bb', '--branch_base', default = None, type = str, help = '')#todo
+    parser.add_argument('-ib', '--internal_branch', default = None, type = str, help = 'The name of the branch in the internal repository.') #todo
+    parser.add_argument('-eb', '--external_branch', default = None, type = str, help = 'The name of the branch in the external repository.')#todo
+    parser.add_argument('-bb', '--branch_base', default = None, type = str, help = 'The base branch name to be base of new branch.')#todo
     
     args = parser.parse_args()
     
@@ -40,14 +40,28 @@ def main():
                                             filter_map_path=args.filter_map)
     elif args.fetch:
         check_urls()
-        if args.prefix is None:
-            parser.error('branch prefix argument is required when using -f/--fetch.')
+        prefix_fetch = not(args.prefix is None)
+        branch_fetch = not((args.internal_branch is None)) or not((args.external_branch is None)) or not((args.branch_base is None))
+        
+        if prefix_fetch:
+            Synchronizer().sync_external_internal_with_prefix(
+                                                internal_repo_url=args.internal, 
+                                                external_repo_url=args.external,
+                                                prefix=args.prefix
+                                                )
+        elif branch_fetch:            
+            if args.internal_branch is None:
+                parser.error('internal_branch argument is required when using -f/--fetch.')
+            elif args.external_branch is None:
+                parser.error('external_branch argument is required when using -f/--fetch.')
+            elif args.branch_base is None:
+                parser.error('branch_base argument is required when using -f/--fetch.')
+            else:
+                Synchronizer().sync_external_internal_with_branch(internal_repo_url=args.internal, external_repo_url=args.external, src_branch_base=args.branch_base, )
+        else:                   
+            parser.error('When using -f/--fetch, one of two following cases is required (1) --prefix \n (2) --internal_branch, --external_branch and --branch_base')
 
-        Synchronizer().sync_external_internal(
-                                            internal_repo_url=args.internal, 
-                                            external_repo_url=args.external,
-                                            prefix=args.prefix
-                                            )
+
 
     else:
         print(get_logo())
