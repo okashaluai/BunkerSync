4a77817f3578ed6b99567d75670df5f9658ff4a0
Luai Okasha <luai.okasha@infineon.com>
2023-05-17 14:32:30 +0200

    packeging preparation!

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from .src.Synchronizer import(
+    sync_internal_external,
+    sync_external_internal_with_prefix,
+    sync_external_internal_with_branch
+)
+from .src.BunkerSync import(
+    main
+)
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("app/Readme.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="BunkerSync",
+    version="0.0.10",
+    description="""BunkerSync simplifies Git repository syncing. 
+                   Sync internal and external repositories with branch filtering and automatic syncing. 
+                   Ideal for developers who need to keep repositories in sync.""",
+    package_dir={"": "app"},
+    packages= find_packages(where="app"),
+    long_description= long_description ,
+    long_description_content_type = "text/markdown",
+    url= "https://github.com/okashaluai/BunkerSync.git",
+    author= "Luai Okasha",
+    author_email= "LuaiOkasha@gmail.com",
+    license="Apache",
+    classifiers=[
+            "License :: OSI Approved Apache License",
+            "Programming Language :: Python :: 3.11", 
+            "Operating System :: OS Independent"
+    ],
+    install_requires = ["git >= 2.40.0"],
+    extra_require={},
+    python_requires = ">=3.11"
+)
